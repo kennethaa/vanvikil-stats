586f3e2d43d62722db7ff4bf6cce16276fc29f0f
Kenneth <qwek.aasan@sportradar.com>
2017-04-16 20:16:39 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  rules: {
+    'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': 0
+  }
+};
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {'Zuzanna\'s Tinder'}
+      </div>
+    );
+  }
+}
+
+export default App;
