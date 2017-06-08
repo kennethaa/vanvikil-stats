d608662c3b9f74a685eba74610be8f12758d1270
Kenneth Aasan <kenaasa@gmail.com>
2017-06-09 01:03:04 +0200

    adds nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import {
   Container,
   Row,
   Col
 } from './styled-components';
+
+const Nav = styled.nav`
+  background: rgba(4, 18, 31, 0.7);
+`;
+const NavItem = styled.a`
+  padding: 1rem;
+  color: #fff;
+`;
+
+const navItems = [
+  {
+    name: 'Hjem'
+  },
+  {
+    name: 'Om'
+  }
+]
 
 type Props = {
   children: ReactElement
@@ -17,6 +35,20 @@ class Navigation extends Component<void, Props, void> {
       <Container>
         <Row>
           <Col>
+            <Nav>
+              <Row>
+                {navItems.map((navItem) =>
+                  <Col
+                    key={navItem.name}
+                    colWidth={100 / navItems.length}
+                  >
+                    <NavItem>
+                      {navItem.name}
+                    </NavItem>
+                  </Col>
+                )}
+              </Row>
+            </Nav>
           </Col>
         </Row>
         {children}
